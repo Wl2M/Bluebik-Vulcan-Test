e32399baf82df2d09666dcefffda8408a1488e9f
Worameth Siriwarin <94208839+Wl2M@users.noreply.github.com>
2025-11-14 10:00:17 +0700

    Add files via upload

---
+++ src/main/java/InternTest/BlueBikTest/BlueBikTestApplication.java
@@ -0,0 +1,13 @@
+package InternTest.BlueBikTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBikTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBikTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InternTest/BlueBikTest/BlueBikTestApplicationTests.java
@@ -0,0 +1,13 @@
+package InternTest.BlueBikTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBikTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
